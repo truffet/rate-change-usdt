cd3953d0dd2ea5213cf9bd38a41886de080323b3
Sleepy <sleepy@Sleepys-MacBook-Air.local>
2024-12-02 17:54:23 +0100

    added z-scores to telegram bot message)

--- src/telegram_client.py
+++ src/telegram_client.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import Bot
 import asyncio
-from datetime import datetime, timezone
+from datetime import datetime
 
 class TelegramBot:
     def __init__(self, bot_token, chat_id):
@@ -24,8 +24,7 @@ class TelegramBot:
 
             # Extract the open and close time from the DataFrame
             open_time = df['open_time'].min()  # Earliest open_time in the DataFrame
-            close_time = df['close_time'].max()  # Latest close_time in the DataFrame (this is currently in int timestamp)
-            print(close_time)
+            close_time = df['close_time'].max()  # Latest close_time in the DataFrame (in int timestamp)
 
             # Convert close_time from int (timestamp) to datetime
             close_time_dt = datetime.utcfromtimestamp(close_time / 1000)  # Convert milliseconds to seconds for datetime
@@ -37,21 +36,28 @@ class TelegramBot:
             # Add timeframe to the message
             full_message = f"Binance Spot USDT Market Recap ({timeframe.upper()} timeframe)\n"
             full_message += f"Open Time: {open_time_str}\nClose Time: {close_time_str}\n"
-            full_message += "Symbol / Rate Change % Open-Close / % Volatility\n"
+            full_message += "Symbol / OC% / HL% / Z-OC / Z-HL / Z-Vol / Z-OC-All / Z-HL-All / Z-Vol-All\n"
+            
             # Sort DataFrame by 'rate_change_open_close' in descending order
             df = df.sort_values(by='rate_change_open_close', ascending=False)
 
-            # Add each symbol and rate change to the message
+            # Add each symbol, rate changes, Z-scores, and cross-pair Z-scores to the message
             for _, row in df.iterrows():
                 rate_change_icon = "🟩" if row['rate_change_open_close'] >= 0 else "🟥"
                 full_message += (
                     f"{rate_change_icon} {row['symbol']} / "
                     f"{row['rate_change_open_close']:.2f}% / "
-                    f"{row['rate_change_high_low']:.2f}%\n"
+                    f"{row['rate_change_high_low']:.2f}% / "
+                    f"{row['z_rate_change_open_close']:.2f} / "
+                    f"{row['z_rate_change_high_low']:.2f} / "
+                    f"{row['z_volume_pair']:.2f} / "
+                    f"{row['z_rate_change_open_close_all_pairs']:.2f} / "
+                    f"{row['z_rate_change_high_low_all_pairs']:.2f} / "
+                    f"{row['z_volume_all_pairs']:.2f}\n"
                 )
 
             # Send the entire message as a single Telegram message asynchronously
-            await self.bot.send_message(chat_id=self.chat_id, text=f"```\n{full_message}\n```", parse_mode='Markdown')
+            await self.bot.send_message(chat_id=self.chat_id, text=f"{full_message}", parse_mode='Markdown')
 
         except Exception as e:
             logging.error(f"Failed to send message: {e}")
